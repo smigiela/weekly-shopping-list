bf6cfb0d2e768b43cba9e0a88ee6b963b0739b13
Daniel Śmigiela <daniel.smigiela@outlook.com>
2021-08-31 18:12:00 +0200

    turn off email veryfication

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Jetstream\HasTeams;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     use HasTeams;
     use HasApiTokens;
