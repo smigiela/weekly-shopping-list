3a3cf3d0a937a604baee83a2d6565a2ac1d207d3
daniel <daniel.smigiela@outlook.com>
2021-09-22 12:27:40 +0200

    added products with categories

--- app/Models/Recipes/Product.php
+++ app/Models/Recipes/Product.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'product_category_id'];
+
+    public function productCategory()
+    {
+        return $this->belongsTo(ProductCategory::class);
+    }
 }
---
+++ database/seeders/ProductCategorySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Recipes\ProductCategory;
+use Illuminate\Database\Seeder;
+
+class ProductCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        ProductCategory::insert(['name' => 'Warzywa i owoce', 'index' => 1]);
+        ProductCategory::insert(['name' => 'Ryby i owoce morza', 'index' => 2]);
+        ProductCategory::insert(['name' => 'Mięsa', 'index' => 3]);
+        ProductCategory::insert(['name' => 'Nabiał', 'index' => 4]);
+        ProductCategory::insert(['name' => 'Produkty zbożowe', 'index' => 5]);
+        ProductCategory::insert(['name' => 'Przyprawy i dodatki', 'index' => 6]);
+        ProductCategory::insert(['name' => 'Słodycze i przekąski', 'index' => 7]);
+        ProductCategory::insert(['name' => 'Napoje', 'index' => 8]);
+        ProductCategory::insert(['name' => 'Higiena osobista', 'index' => 9]);
+        ProductCategory::insert(['name' => 'Dom', 'index' => 10]);
+    }
+}
